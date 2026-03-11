5e1187a8631c3d6d9662ebec8ba1e2b78c30e369
abhiram3046 <abhiramjampana@gmail.com>
2026-03-11 12:45:40 +0000

    Version:1.2

--- app/app.py
+++ app/app.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD Kubernetes Project Running!!!"
+    return """<html>
+    <head>
+        <title>DevOps Project</title>
+        <style>
+            body {
+                background-color: black;
+                color: white;
+                font-family: Arial, sans-serif;
+                text-align: center;
+                margin-top: 200px;
+            }
+            h1 {
+                font-size: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>🚀 DevOps CI/CD Kubernetes Project Running 🟢🚀🟢🚀🟢🚀</h1>
+        <p>Powered by Docker + Kubernetes + ArgoCD</p>
+    </body>
+    </html>"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
